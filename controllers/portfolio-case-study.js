aea5174cf9a9ac60949f333466f2d86b0c9fa111
Sarah Adewale <saraphynaadewale@gmail.com>
2023-09-22 01:00:01 +0100

    completed layout for all pages

--- controllers/portfolio-case-study.js
+++ controllers/portfolio-case-study.js
@@ -1,7 +1,4 @@
 module.exports = {
-    getIndex: (req, res) => {
-        res.render('index.ejs')
-    },
     getPortfolioCaseStudy: (req, res) => {
         res.render('portfolio-case-study.ejs')
     },
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const homeController = require('../controllers/home')
+const portfolioCaseStudyController = require('../controllers/portfolio-case-study')
 
 
 router.get('/', homeController.getIndex)
+router.get('/portfolio-case-study', portfolioCaseStudyController.getPortfolioCaseStudy)
 
 module.exports = router
